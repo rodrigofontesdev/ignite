dec550f2d09342ac79a0b17bfc72db1c2f81110d
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-05 22:22:01 -0300

    02: story button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,30 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+import { ComponentProps } from 'react'
 
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  fontWeight: '$bold',
+  backgroundColor: '$ignite300',
+  color: '$white',
+  borderRadius: '$md',
+  border: 0,
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
